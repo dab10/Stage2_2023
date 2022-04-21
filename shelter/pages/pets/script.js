28946452946f4749ab2f2930839bb8e416236fe5
dab10 <dab10@list.ru>
2022-04-22 01:05:45 +0300

    feat: made some carousel

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -8,11 +8,11 @@ const backgroundGrey = document.querySelector('.grey-pets');
 const navLinkPets = document.querySelector('.navigation-pets');
 const navLogo = document.querySelector('.logo-pets-double')
 
-navLinks.forEach((el) => el.classList.toggle('is-active-menu'));  //change text color menu burger
+//navLinks.forEach((el) => el.classList.toggle('is-active-menu'));  //change text color menu burger
 
 function toggleMenu() {
     hamburgerPets.classList.toggle('is-active');                    //rotate burger
-    hamburgerCenterLine.classList.toggle('is-active-center-line'); //change color center line menu burger
+ //   hamburgerCenterLine.classList.toggle('is-active-center-line'); //change color center line menu burger
     navPets.classList.toggle('open-pets');
     navLogo.classList.toggle('open-logo');
     backgroundGrey.classList.toggle('overplay-pets');
@@ -25,11 +25,11 @@ hamburgerPets.addEventListener('click', toggleMenu);
 function closeMenu(event) {
     if (event.target.classList.contains('navigation-pets__link') || event.target.classList.contains('grey-pets')) {
         hamburgerPets.classList.remove('is-active');
-        hamburgerCenterLine.classList.remove('is-active-center-line');
+     //   hamburgerCenterLine.classList.remove('is-active-center-line');
         navPets.classList.remove('open-pets');
         navLogo.classList.remove('open-logo');
         backgroundGrey.classList.remove('overplay-pets');
-        document.body.classList.close('hidden-pets');
+        document.body.classList.remove('hidden-pets');
     }    
 }
 
