9caf76661fd4beed1e9d0c18fe4cbee9b6610039
dab10 <dab10@list.ru>
2022-07-02 00:49:30 +0300

    feat: upgrade sources.js to sources.ts without errors

--- migration-newip-to-ts/src/types/index.ts
+++ migration-newip-to-ts/src/types/index.ts
@@ -7,14 +7,18 @@ export interface INewsApi {
     language: string;
     country: string;
 }
+
 export interface INews {
-    source: {
-        name: string;
-    };
+    source: ISource;
     author: string;
     publishedAt: string;
     title: string;
     description: string;
     url: string;
     urlToImage: string;
+}
+
+export interface ISource {
+    id: string;
+    name: string;
 }
