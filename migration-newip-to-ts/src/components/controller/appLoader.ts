5694a41f41888e737a6790a8a7f47e0ee32a3a4f
dab10 <dab10@list.ru>
2022-07-03 22:16:06 +0300

    feat: change url in fetch for work on gh-pages

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '51504b36bb2f4859a90e65e4990490ec', //51504b36bb2f4859a90e65e4990490ec 696e176682f349a7aa615bfb3c53d11c
         });
     }
