13ad8ba0bf3b8b859f6ed30582f47a6abb1fc215
dab10 <dab10@list.ru>
2022-07-23 12:42:06 +0300

    feat: add some test

--- online-store/tests/reset-all.test.ts
+++ online-store/tests/reset-all.test.ts
@@ -1,10 +1,16 @@
-import Filter from '../src/components/filter/filter';
 import ResetAll from '../src/components/filter/reset-all';
-import data from '../src/components/goods/goods.json';
-import { IFilter, filterWordsEmpty } from '../src/types/index';
+import { filterWordsEmpty } from '../src/types/index';
 
 document.body.innerHTML = `
 <span class="counter">0</span>
+<div class="filter-by-name">
+    Производитель:
+    <div class="filter-by-name__company">
+        <button class="button apple">Apple</button>
+        <button class="button samsung">Samsung</button>
+        <button class="button xiaomi">Xiaomi</button>
+    </div>
+</div>
 <div class="reset">
     <button class="reset-filter">Сброс фильтров</button>
     <button class="reset-all">Сброс настроек</button>
@@ -34,5 +40,13 @@ describe('Class ResetAll', () => {
         const resetAll = document.querySelector('.reset-all') as HTMLButtonElement;
         resetAll.click();
         expect(classResetAll.getResetAllFilterWords()).toMatchObject(filterWordsEmpty);
+    });
+    test('Check contain filterWords', () => {
+        const filterByValue = document.querySelector('.apple') as HTMLButtonElement;
+        filterByValue.click();
+        const resetAll = document.querySelector('.reset-all') as HTMLButtonElement;
+        resetAll.click();
+        const filterWordsEmptyFromResetAll = classResetAll.getResetAllFilterWords();
+        expect(filterWordsEmptyFromResetAll.cameraValue).toHaveLength(0);
     });
 });
