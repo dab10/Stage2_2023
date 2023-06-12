2b07a8e3ddcd15a0f96f4b86e00bfa3f062df7e5
dab10 <dab10@list.ru>
2023-06-12 19:22:45 +0300

    fix: fix name server

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '51504b36bb2f4859a90e65e4990490ec',
         });
     }
