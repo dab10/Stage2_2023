a43f30996abd3874bad873ad92fb3058368082cc
dab10 <dab10@list.ru>
2023-07-03 21:57:49 +0300

    fix: fix some styles

--- rs-css/src/application/modalPageView.ts
+++ rs-css/src/application/modalPageView.ts
@@ -5,11 +5,17 @@ import style from './modalPageView.css';
 export class ModalPage extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div');
-    const modalPage = new Control<HTMLDialogElement>(this.node, 'dialog', style['dialog'], 'Congratulation');
+    const modalPage = new Control<HTMLDialogElement>(
+      this.node,
+      'dialog',
+      style['dialog'],
+      'Congratulation! All levels complete!'
+    );
     const modalPageButton = new Control<HTMLButtonElement>(modalPage.node, 'button', '', 'ok');
     modalPage.node.showModal();
     modalPageButton.node.onclick = () => {
       modalPage.node.close();
+      modalPage.destroy();
     };
   }
 }
