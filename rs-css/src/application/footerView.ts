df65e733069f6ead37af72ce568a6f2ec3b618f0
dab10 <dab10@list.ru>
2023-07-06 23:59:00 +0300

    refactor: add empty line in footerView

--- rs-css/src/application/footerView.ts
+++ rs-css/src/application/footerView.ts
@@ -6,6 +6,7 @@ import style from './footerView.css';
 export class FooterView extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', style['footer']);
+
     const footer = new Control(this.node);
     footer.node.outerHTML = footerStart;
   }
