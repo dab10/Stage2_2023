a8fad32b1484c1b84100a6655b8fa78fb9bb5ba9
dab10 <dab10@list.ru>
2023-07-07 00:18:27 +0300

    refactor: delete unused methods and rename constants

--- rs-css/src/application/gameHTMLView.ts
+++ rs-css/src/application/gameHTMLView.ts
@@ -6,21 +6,18 @@ import { AnimatedControl } from '../common/animatedControl';
 import style from './cssEditorView.css';
 
 export class GameHtmlView extends AnimatedControl {
-  onHover!: (arg0: Control) => void;
-  onHoverOut!: (arg0: Control) => void;
-
   constructor(parentNode: HTMLElement, gameData: GameData[], state: GameState) {
     super(parentNode, 'pre', { default: '', hidden: style['shake'] });
 
     const gameHtmlViewField = new Control(this.node, 'code', 'language-html prettyprint');
-    const elTableStart = new Control(
+    const elementTableStart = new Control(
       gameHtmlViewField.node,
       'div',
       '',
       gameData[state.data.currentLevel].htmlCode.split('\r\n')[0]
     );
 
-    elTableStart.node.id = 'editor';
+    elementTableStart.node.id = 'editor';
 
     gameData[state.data.currentLevel].htmlCode
       .split('\r\n')
@@ -29,7 +26,7 @@ export class GameHtmlView extends AnimatedControl {
         const element = new Control(gameHtmlViewField.node, 'div', 'white-font', item);
       });
 
-    const elTableEnd = new Control(
+    const elementTableEnd = new Control(
       gameHtmlViewField.node,
       'div',
       '',
