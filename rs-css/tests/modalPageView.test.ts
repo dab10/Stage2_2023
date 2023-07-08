8632012574480d3fb7da48a7b8b85dcb0bd6e37a
dab10 <dab10@list.ru>
2023-07-09 00:41:27 +0300

    refactor: add empty lines, delete magic number, add function for duplicate code in tests

--- rs-css/tests/modalPageView.test.ts
+++ rs-css/tests/modalPageView.test.ts
@@ -3,6 +3,8 @@ import { GameState } from '../src/application/gameState';
 import gameData from '../src/assets/json/gameData.json';
 import { Application } from '../src/application/application';
 
+const TIME_ANIMATION = 1000;
+
 beforeAll((done) => {
   global.fetch = jest.fn(() =>
     Promise.resolve({
@@ -18,50 +20,68 @@ beforeAll((done) => {
 describe('Application tests', () => {
   test('Check create all button', () => {
     const buttonElements = document.querySelectorAll('.level-button');
+
     expect(buttonElements).toHaveLength(gameData.length);
   });
+
   test('Check create reset button', () => {
     const resetButton = document.querySelector('.reset-button') as HTMLButtonElement;
+
     expect(resetButton).not.toBeUndefined();
   });
+
   test('Check choose level 5', () => {
-    let button = document.querySelector(
-      'body > div > div.game-level > div > div:nth-child(5) > button'
-    ) as HTMLButtonElement;
+    const pickButton = () =>
+      document.querySelector('body > div > div.game-level > div > div:nth-child(5) > button') as HTMLButtonElement;
+
+    let button = pickButton();
+
     button.click();
-    button = document.querySelector(
-      'body > div > div.game-level > div > div:nth-child(5) > button'
-    ) as HTMLButtonElement;
+
+    button = pickButton();
+
     expect(button.classList.contains('active-level-button')).toBe(true);
   });
+
   test('Check use hint the 5 level', () => {
     const helpButton = document.querySelector('.help-button') as HTMLButtonElement;
+
     helpButton.click();
+
     const hintSymbol = document.querySelector(
       'body > div > div.game-level > div > div:nth-child(5) > div.complete-level-symbol-with-hint.complete-level'
     ) as HTMLButtonElement;
+
     expect(hintSymbol.classList.contains('complete-level')).toEqual(true);
   });
+
   test('Check text in input after press help button', async () => {
     const helpButton = document.querySelector('.help-button') as HTMLButtonElement;
     const inputField = document.querySelector(
       'body > div > div.game-editor > div > form > input.input-field'
     ) as HTMLInputElement;
+
     helpButton.click();
+
     setTimeout(() => {
       expect(inputField.value).toMatch('plate');
-    }, 1000);
+    }, TIME_ANIMATION);
   });
+
   test('Check move to next level', async () => {
     const enterButton = document.querySelector('.like-button ') as HTMLInputElement;
+
     enterButton.click();
+
     setTimeout(() => {
       const levelButton = document.querySelector(
         'body > div > div.game-level > div > div:nth-child(6) > button'
       ) as HTMLButtonElement;
+
       expect(levelButton.classList.contains('active-level-button')).toBeTruthy();
-    }, 1000);
+    }, TIME_ANIMATION);
   });
+
   test('Check moving element', async () => {
     const inputField = document.querySelector(
       'body > div > div.game-editor > div > form > input.input-field'
@@ -69,11 +89,14 @@ describe('Application tests', () => {
     inputField.value = 'plate';
     const enterButton = document.querySelector('.like-button ') as HTMLInputElement;
     const plate = document.querySelector('body > div > div.game-header > div > div > div > plate') as HTMLElement;
+
     enterButton.click();
+
     setTimeout(() => {
       expect(plate).toBeNull();
-    }, 1000);
+    }, TIME_ANIMATION);
   });
+
   test('Check animation incorrect answer', async () => {
     const inputField = document.querySelector(
       'body > div > div.game-editor > div > form > input.input-field'
@@ -81,21 +104,29 @@ describe('Application tests', () => {
     inputField.value = 'plate';
     const enterButton = document.querySelector('.like-button ') as HTMLInputElement;
     const containerEditor = document.querySelector('body > div > div.game-editor > div') as HTMLInputElement;
+
     enterButton.click();
+
     expect(containerEditor.classList.contains('shake')).toBeDefined();
   });
+
   test('Check reset progress', async () => {
     const resetButton = document.querySelector('.reset-button') as HTMLButtonElement;
+
     resetButton.click();
+
     const firstLevelButton = document.querySelector(
       'body > div > div.game-level > div > div:nth-child(1) > button'
     ) as HTMLButtonElement;
+
     expect(firstLevelButton.classList.contains('active-level-button')).toBe(true);
   });
+
   test('Check strobe animation on element', async () => {
     const elementWithAnimation = document.querySelector(
       'body > div > div.game-header > div > div > box:nth-child(1)'
     ) as HTMLElement;
+
     expect(elementWithAnimation.classList.contains('strobe')).not.toBeFalsy();
   });
 });
