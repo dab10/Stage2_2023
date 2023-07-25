65aedd24f575a4eb41ef09b5267acc3d635eb409
dab10 <dab10@list.ru>
2023-07-25 22:43:43 +0300

    refactor: delete short names, add brackets for if blocks

--- async-race/src/components/controller/controller.ts
+++ async-race/src/components/controller/controller.ts
@@ -44,33 +44,49 @@ class Controller {
     const buttonNextWinners = document.querySelector('.pagination-winners__next') as HTMLButtonElement;
     const buttonPrevWinners = document.querySelector('.pagination-winners__prev') as HTMLButtonElement;
 
-    main.addEventListener('click', (e) => this.listenButtonsCar(e));
-    createButton.addEventListener('click', (e) => this.editCar.createNewCar(e));
+    main.addEventListener('click', (event) => this.listenButtonsCar(event));
+    createButton.addEventListener('click', (event) => this.editCar.createNewCar(event));
     buttonNext.addEventListener('click', this.pagination.moveNext);
     buttonPrev.addEventListener('click', this.pagination.movePrev);
     buttonNextWinners.addEventListener('click', this.pagination.moveNextWinners);
     buttonPrevWinners.addEventListener('click', this.pagination.movePrevWinners);
     raceAll.addEventListener('click', this.animation.raceAll);
     raceReset.addEventListener('click', this.animation.raceReset);
-    winners.addEventListener('click', (e) => View.changePage(e));
-    garage.addEventListener('click', (e) => View.changePage(e));
+    winners.addEventListener('click', View.changePage);
+    garage.addEventListener('click', View.changePage);
     popup.addEventListener('click', View.popupHidden);
-    winnersTable.addEventListener('click', (e) => this.listenButtonsSortingTable(e));
+    winnersTable.addEventListener('click', (event) => this.listenButtonsSortingTable(event));
     generateCars.addEventListener('click', this.editCar.generateCars);
   }
 
-  private listenButtonsCar(e: Event): void {
-    if ((e.target as HTMLButtonElement).classList.contains('start-stop-car__start-button')) this.animation.animatePosition(e);
-    if ((e.target as HTMLButtonElement).classList.contains('start-stop-car__stop-button')) this.animation.animateStop(e);
-    if ((e.target as HTMLButtonElement).classList.contains('car-buttons__select')) this.editCar.editCar(e);
-    if ((e.target as HTMLButtonElement).classList.contains('car-buttons__remove')) this.editCar.removeCar(e);
+  private listenButtonsCar(event: Event): void {
+    if ((event.target as HTMLButtonElement).classList.contains('start-stop-car__start-button')) {
+      this.animation.animatePosition(event);
+    }
+    if ((event.target as HTMLButtonElement).classList.contains('start-stop-car__stop-button')) {
+      this.animation.animateStop(event);
+    }
+    if ((event.target as HTMLButtonElement).classList.contains('car-buttons__select')) {
+      this.editCar.editCar(event);
+    }
+    if ((event.target as HTMLButtonElement).classList.contains('car-buttons__remove')) {
+      this.editCar.removeCar(event);
+    }
   }
 
-  private listenButtonsSortingTable(e: Event): void {
-    if ((e.target as HTMLElement).classList.contains('sort-by-wins_asc')) this.tableWinners.sorting('wins', 'asc');
-    if ((e.target as HTMLElement).classList.contains('sort-by-wins_desc')) this.tableWinners.sorting('wins', 'desc');
-    if ((e.target as HTMLElement).classList.contains('sort-by-time_asc')) this.tableWinners.sorting('time', 'asc');
-    if ((e.target as HTMLElement).classList.contains('sort-by-time_desc')) this.tableWinners.sorting('time', 'desc');
+  private listenButtonsSortingTable(event: Event): void {
+    if ((event.target as HTMLElement).classList.contains('sort-by-wins_asc')) {
+      this.tableWinners.sorting('wins', 'asc');
+    }
+    if ((event.target as HTMLElement).classList.contains('sort-by-wins_desc')) {
+      this.tableWinners.sorting('wins', 'desc');
+    }
+    if ((event.target as HTMLElement).classList.contains('sort-by-time_asc')) {
+      this.tableWinners.sorting('time', 'asc');
+    }
+    if ((event.target as HTMLElement).classList.contains('sort-by-time_desc')) {
+      this.tableWinners.sorting('time', 'desc');
+    }
   }
 }
 
